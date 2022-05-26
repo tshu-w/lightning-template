d02fe03235f2d7518e59e049364a6dc82a6f2921
Tianshu Wang <wang@tianshu.me>
2022-05-26 09:11:04 +0800

    Drop unnessary setup

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,7 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((magit-mode . ((eval . (setq-local magit-git-environment
-                                    (append magit-git-environment
-                                            '("CONDA_DEFAULT_ENV=template"))))))
- (python-mode . ((eval . (setq eglot-workspace-configuration
+((python-mode . ((eval . (setq eglot-workspace-configuration
                                `((:python
                                   :venvPath ,(expand-absolute-name "~/.local/share/conda/envs")
                                   :analysis (:diagnosticMode
