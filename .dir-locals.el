9618ae30daa18e0777d9707de1f00cb0ca87286b
Tianshu Wang <wang@tianshu.me>
2022-08-15 13:39:45 +0800

    Switch back to lsp-mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((python-mode . ((eval . (setq eglot-workspace-configuration
-                               `((:python
-                                  :venvPath ,(expand-absolute-name "~/.local/share/conda/envs")
-                                  :analysis (:diagnosticMode
-                                             "openFilesOnly"
-                                             :stubPath
-                                             ,(expand-absolute-name "~/.local/lib/python-type-stubs")))))))))
+((python-mode . ((lsp-pyright-venv-path . "~/.local/share/conda/envs")
+                 (lsp-pyright-stub-path . "~/.local/lib/python-type-stubs"))))
