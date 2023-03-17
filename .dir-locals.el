22c13504d39a11a7e72dc245e796b2b28f50eb0c
Tianshu Wang <wang@tianshu.me>
2023-03-17 19:47:46 +0800

    Update .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,10 @@
-;;; Directory Local Variables
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((python-mode . ((lsp-pyright-venv-path . "~/.local/share/conda/envs")
-                 (lsp-pyright-stub-path . "~/.local/lib/python-type-stubs"))))
+((python-base-mode . ((eval . (setq eglot-workspace-configuration
+                                    `((:python
+                                       :venvPath ,(expand-absolute-name "~/.local/share/conda/envs")
+                                       :analysis (:diagnosticMode
+                                                  "openFilesOnly"
+                                                  :stubPath
+                                                  ,(expand-absolute-name "~/.local/lib/python-type-stubs")))))))))
