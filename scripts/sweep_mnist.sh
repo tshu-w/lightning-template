d3261a5daa8b7b30d6e57339be211d56278a68d5
Tianshu Wang <wang@tianshu.me>
2023-07-12 08:46:59 +0000

    Add sweep_cli

--- scripts/sweep_mnist.sh
+++ scripts/sweep_mnist.sh
@@ -1,1 +1,1 @@
-python scripts/sweep_mnist.py fit --config_files "[configs/mnist.yaml]" --batch_sizes "[16, 32]" --seeds "[42, 123]" --gpus_per_trial 0.5
+bash ./scripts/sweep --config configs/sweep_mnist.yaml
